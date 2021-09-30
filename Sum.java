52dbb00547ac11b3fa55425f2bb1a99420f1becd
Ajay Khandge <ajaykhandge88@gmail.com>
2021-09-30 11:05:02 +0530

    Sum function added

--- Sum.java
+++ Sum.java
@@ -2,6 +2,11 @@ class Sum{
     public static void main(String[]args){
         System.out.println(1+2);
          System.out.println(1+3);
+         System.out.println(sum(1,5));
         
+    }
+
+    public static int sum(int a,int b){
+        return a+b;
     }
 }
